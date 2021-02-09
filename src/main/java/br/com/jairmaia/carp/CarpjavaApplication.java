e636e11552b41ea4a1b2ccccf2c5aa6e44b229fc
Jair <jairmaiag@gmail.com>
2021-02-09 10:09:15 -0300

    Primeiro commit

---
+++ src/main/java/br/com/jairmaia/carp/CarpjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jairmaia.carp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jairmaia/carp/CarpjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jairmaia.carp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
